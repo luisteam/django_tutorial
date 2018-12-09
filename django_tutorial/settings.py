024f50bfaf9aaa08fe64558318fd736405d38dd0
luisteam <luis_rayita@hotmail.es>
2018-12-09 20:15:39 +0100

    Subida14

--- django_tutorial/settings.py
+++ django_tutorial/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'd9gr^r=$cyhf$qq&wx%up$_am0x17+2-o82*isvhkqy_uzutyp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
--- django_tutorial/urls.py
+++ django_tutorial/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import url, include
 
 urlpatterns = [
+    url(r'^', include('polls.urls')),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
